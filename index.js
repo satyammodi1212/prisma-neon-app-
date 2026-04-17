2cbb7a2c61859a6edefb68ce2c6158bf945ab2ce
satyam <ankitmodi117@gmail.com>
2026-04-17 20:27:24 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import "dotenv/config";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  // Create
+  const user = await prisma.user.create({
+    data: {
+      name: "Satyam",
+      email: "ankitmodi117@gmail.com",
+    },
+  });
+  console.log("Created:", user);
+
+  // Read
+  const users = await prisma.user.findMany();
+  console.log("Users:", users);
+}
+
+main()
+  .catch((e) => console.error(e))
+  .finally(() => prisma.$disconnect());
